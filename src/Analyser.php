89c8c4cd1869c503e04a8504285da51d4daf677b
Иван Зорин <creator@localzet.ru>
2023-07-29 14:46:45 +0500

    Update Analyser.php

--- src/Analyser.php
+++ src/Analyser.php
@@ -55,23 +55,25 @@ class Analyser
             $this->headers['client-ip'] ??
             $this->headers['x-client-ip'] ??
             $this->headers['remote-addr'] ??
-            $this->headers['via'];
+            $this->headers['via'] ?? null;
 
         $ip = filter_var($ip, FILTER_VALIDATE_IP) ? $ip : null;
 
-        try {
-            $maxmind = new Reader(__DIR__ . '/../data/GeoLite2-City.mmdb');
-            $record = $maxmind->get($ip);
-        } catch (Throwable) {
-            /* :) */
+        if ($ip) {
+            try {
+                $maxmind = new Reader(__DIR__ . '/../data/GeoLite2-City.mmdb');
+                $record = $maxmind->get($ip);
+            } catch (Throwable) {
+                /* :) */
+            }
+
+            $this->data->location->city = $record['city']['names']['ru'] ?? $record['city']['names']['en'] ?? null;
+            $this->data->location->country = $record['country']['names']['ru'] ?? $record['country']['names']['en'] ?? null;
+            $this->data->location->country_code = $record['country']['iso_code'] ?? null;
+            $this->data->location->timezone = $record['location']['time_zone'] ?? null;
+            $this->data->location->subdivisions = $record['subdivisions'] ?? null;
         }
-
-        $this->data->location->city = $record['city']['names']['ru'] ?? $record['city']['names']['en'] ?? null;
-        $this->data->location->country = $record['country']['names']['ru'] ?? $record['country']['names']['en'] ?? null;
-        $this->data->location->country_code = $record['country']['iso_code'] ?? null;
-        $this->data->location->timezone = $record['location']['time_zone'] ?? null;
-        $this->data->location->subdivisions = $record['subdivisions'] ?? null;
-
+        
         return $this;
     }
 }
